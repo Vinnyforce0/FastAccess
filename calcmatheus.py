7d78c6ed2cd09f62b5491a245e91909907395407
vinnyforce0 <jorgevinny2008@gmail.com>
2025-10-01 14:17:37 -0300

    mais um

--- calcmatheus.py
+++ calcmatheus.py
@@ -7,8 +7,16 @@ root.geometry("800x600")
 def exemplo():
     label.config(text="Botão clicado!")
 
-button = tk.Button(root, text="Selecionar Item").place(x=500,y=30)
-label = tk.Label(root, text="Bem-vindo à minha aplicação!").place(x=500,y=10)
+col1 = 350
+col2 = 600
+col3 = 200
+col4 = 200
+SubValor = 0
+DesValor = 0
+button = tk.Entry(root, textvariable=SubValor).place(x=col1,y=35)
+button = tk.Entry(root, textvariable=DesValor).place(x=col2,y=35)
+label = tk.Label(root, text="Sub-Total").place(x=col1,y=10)
+label = tk.Label(root, text="Desconto").place(x=col2,y=10)
 listbox = tk.Listbox(root, height=20, width=50).place(x=10,y=10)
 
 root.mainloop()
