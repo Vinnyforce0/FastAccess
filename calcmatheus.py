01fdc2248876001d7233f846f91185d9a90cdac7
vinnyforce0 <jorgevinny2008@gmail.com>
2025-10-03 15:17:19 -0300

    commit +

--- calcmatheus.py
+++ calcmatheus.py
@@ -29,8 +29,8 @@ tobotao = tk.Entry(root, textvariable=TotalValor, width=24, font=("Arial", 25, "
 label = tk.Label(root, text="Total").place(x=col1,y=(40 * (4.1 * 1))- 10)
 
 #linha 3
-Subotao = tk.Entry(root, textvariable=ResValor, width=12, font=("Arial", 20, "")).place(x=col1,y=(40 * (4.1 * 2)))
-desbotao = tk.Entry(root, textvariable=TroValor, width=12, font=("Arial", 20, "")).place(x=col2,y=(40 * (4.1 * 2)))
+Resotao = tk.Entry(root, textvariable=ResValor, width=12, font=("Arial", 20, "")).place(x=col1,y=(40 * (4.1 * 2)))
+Trobotao = tk.Entry(root, textvariable=TroValor, width=12, font=("Arial", 20, "")).place(x=col2,y=(40 * (4.1 * 2)))
 
 label = tk.Label(root, text="Valor Recebido").place(x=col1,y=(40 * (4.1 * 2)) - 25)
 label = tk.Label(root, text="Troco").place(x=col2,y=(40 * (4.1 * 2))- 25)
@@ -39,7 +39,6 @@ label = tk.Label(root, text="Troco").place(x=col2,y=(40 * (4.1 * 2))- 25)
 opcoes = ["Débito", "Crédito", "Dinheiro", "Voucher", "Cartão Presente", "Pix"]
 combo = ttk.Combobox(root, values=opcoes, width=27).place(x=col1,y=410)
 label = tk.Label(root, text="Formas de Pagamento").place(x=col1,y=385)
-
 
 #o famoso listbox
 listbox = tk.Listbox(root, height=20, width=50, listvariable=PayValor).place(x=10,y=40)
@@ -55,6 +54,9 @@ imagemlogo = tk.PhotoImage(file="ibagens/logi.png")
 imagemlogo = imagemlogo.subsample(4,4)
 label = tk.Label(root, image=imagemlogo, width=300, height=200).place(x=10,y=380)
 
+#codigo de barras
+label = tk.Label(root, text="Código de rabas").place(x=col1,y=450)
+label = tk.Label(root, text="01100101010010110101011001").place(x=col1,y=480)
 
 
 root.mainloop()
